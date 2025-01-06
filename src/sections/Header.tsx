ba1aa36e2cbd86e5aebddd09f26f8d5ae65628d2
Your Name <you@example.com>
2025-01-06 11:04:00 +0800

    update on header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,19 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from '@/assets/arrow-right.svg'
+import Logo from '@/assets/logosaas.png'
+import Image from 'next/image'
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-center items-center py-3 bg-black text-white">
+        <div className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center" />
+        </div>
+      </div>
+      <div>
+        <Image src={Logo} alt='Saas Logo' height={40} width={40} />
+      </div>
+    </header>
+  )
 };
